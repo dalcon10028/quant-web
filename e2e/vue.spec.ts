25286c0dfc7a281bf1ec1963770f4ad55ae6b477
corn <dalcon10280@gmail.com>
2024-05-20 00:17:30 +0900

    test playwright

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -2,7 +2,9 @@ import { test, expect } from '@playwright/test'
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
+test('visits the app root url', async ({ page }, testInfo) => {
   await page.goto('/')
+  const screenshot = await page.screenshot()
+  await testInfo.attach('screenshot', { body: screenshot, contentType: 'image/png' })
   await expect(page.locator('div.greetings > h1')).toHaveText('You did it!')
 })
