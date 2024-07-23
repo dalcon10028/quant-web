571b82eafe0547a282e56399211f21bbb71051a7
corn <dalcon10280@gmail.com>
2024-07-24 00:32:24 +0900

    chore: install prime vue

---
+++ src/app/main.ts
@@ -0,0 +1,15 @@
+import './styles/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+
+import App from './App.vue'
+import router from '@/app/providers/router'
+import { enableMocking } from '@/mocks/enable-mocking'
+
+const app = createApp(App).use(createPinia()).use(router).use(PrimeVue)
+
+enableMocking().then(() => {
+  app.mount('#app')
+})
---
+++ src/pages/home/ui/home-page.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <h1>asdfasdfsd</h1>
+  <Button label="Submit" />
+</template>
